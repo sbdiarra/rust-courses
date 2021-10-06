0eed719ab73a15f8f8b446a79777555f4b0184f6
Sekou Diarra <sbdiarra.comet@tf1.fr>
2021-10-06 22:16:49 +0200

    rmv target folders

--- chap6/target/debug/chap6.d
+++
@@ -1,1 +0,0 @@
-/home/sekou/Dev/rust-courses/chap6/target/debug/chap6: /home/sekou/Dev/rust-courses/chap6/src/main.rs
--- chap6/target/debug/deps/chap6-81f67b478e74e0b3.d
+++
@@ -1,5 +0,0 @@
-/home/sekou/Dev/rust-courses/chap6/target/debug/deps/chap6-81f67b478e74e0b3: src/main.rs
-
-/home/sekou/Dev/rust-courses/chap6/target/debug/deps/chap6-81f67b478e74e0b3.d: src/main.rs
-
-src/main.rs:
