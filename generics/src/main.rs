bce7dc38faa082925c628fd46c4551e33e3c3292
Sekou Diarra <sbdiarra.comet@tf1.fr>
2021-12-06 16:24:44 +0100

    wip3

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -40,6 +40,18 @@ fn largest_gen<T>(list: &[T]) -> T {
     largest
 }
 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     // let number_list = vec![34, 50, 25, 100, 65];
     //
